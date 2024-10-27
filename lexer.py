544951eb31e2cd229dba4fc1827c96f117e226c8
Isaac López <124508722+Isaac-lop2@users.noreply.github.com>
2024-10-26 18:20:27 -0600

    Update lexer.py
    Update Phase 1

--- lexer.py
+++ lexer.py
@@ -4,22 +4,22 @@ class Lexer:
         self.tokens = {}
         self.errors = []
 
+        self.palabras_reservadas = [
+            'si', 'mientras', 'hacer', 'entonces', 'entero', 'decimal', 'booleano',
+            'cadena', 'sino', 'verdadero', 'falso'
+        ]
 
-        self.palabras_reservadas = ['si', 'mientras', 'hacer', 'entonces']
-
-
-        self.operadores = ['+', '-', '*', '/', '=', '==', '!=', '<', '>', '<=', '>=']
-
-
-        self.simbolos = ['(', ')']
+        self.operadores = ['+', '-', '*', '/', '%', '=', '==', '!=', '<', '>', '<=', '>=']
+        self.simbolos = ['(', ')', '{', '}', '"', ';']
 
     def is_identificador(self, palabra):
-
+        # Un identificador comienza con letra o guion bajo y sigue con letras o dígitos
         if palabra[0].isalpha() or palabra[0] == '_':
             return all(c.isalnum() or c == '_' for c in palabra)
         return False
 
     def is_numero(self, palabra):
+        # Número en base a `DD*` (solo dígitos)
         return palabra.isdigit()
 
     def is_operador(self, palabra):
@@ -34,14 +34,13 @@ class Lexer:
     def analyze(self):
         linea_actual = 0
 
-
         for linea in self.content:
             linea_actual += 1
             palabras = self.tokenize(linea)
 
             for palabra in palabras:
                 if self.is_palabra_reservada(palabra):
-                    self.agregar_token(palabra, 'RESERVADA')
+                    self.agregar_token(palabra, 'PALABRA RESERVADA')
                 elif self.is_identificador(palabra):
                     self.agregar_token(palabra, 'IDENTIFICADOR')
                 elif self.is_numero(palabra):
@@ -49,7 +48,7 @@ class Lexer:
                 elif self.is_operador(palabra):
                     self.agregar_token(palabra, 'OPERADOR')
                 elif self.is_simbolo(palabra):
-                    self.agregar_token(palabra, 'SIMBOLO')
+                    self.agregar_token(palabra, 'SIGNO')
                 else:
                     self.errors.append({
                         'linea': linea_actual,
@@ -59,14 +58,12 @@ class Lexer:
         return self.tokens, self.errors
 
     def tokenize(self, linea):
-
         tokens = []
         token_actual = ''
         i = 0
 
         while i < len(linea):
             char = linea[i]
-
 
             if char in ('=', '!', '<', '>'):
                 if i + 1 < len(linea) and linea[i + 1] == '=':
@@ -94,7 +91,6 @@ class Lexer:
         return tokens
 
     def agregar_token(self, token, tipo):
-
         if token not in self.tokens:
             self.tokens[token] = {'tipo': tipo, 'cantidad': 1}
         else:
